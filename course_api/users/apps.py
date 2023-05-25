e6f925263161a76f3df1a43d44174d700b32dec1
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-05-25 12:47:13 +0530

    setting up

--- course_api/users/apps.py
+++ course_api/users/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
 class UsersConfig(AppConfig):
     name = "course_api.users"
     verbose_name = _("Users")
